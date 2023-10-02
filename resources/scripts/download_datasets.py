956523d4e8300edb0b11116e220366680ecb4cff
강지혁 <wlgur7238@snu.ac.kr>
2023-10-03 00:35:38 +0900

    use chunking

--- resources/scripts/download_datasets.py
+++ resources/scripts/download_datasets.py
@@ -5,6 +5,14 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 from tqdm import tqdm
 
+import io
+from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.discovery import build
+from google.oauth2.credentials import Credentials
+
+# Load your credentials
+creds = None
+
 try:
     with open('/mount/credentials.json', 'r') as f:
         with open('credentials.json', 'w') as f2:
@@ -12,6 +20,9 @@ try:
 except Exception as e:
     print(e)
     pass
+
+with open('credentials.json', 'r') as f:
+    creds = Credentials.from_authorized_user_file('credentials.json')
 
 gauth = GoogleAuth()
 gauth.LoadCredentialsFile(os.path.realpath("credentials.json"))
@@ -26,6 +37,33 @@ def is_folder(file):
 
 
 def download(file, root_dir, dry_run):
+    if dry_run:
+        print('Dry run, skipping download... {}'.format(file['title']))
+        return
+
+    # Build the Drive API service
+    service = build('drive', 'v3', credentials=creds)
+    request = service.files().get_media(fileId=file['id'])
+
+    # Prepare a local file to write the data
+    path = os.path.join(root_dir, file['title'])
+    path = os.path.join('/input', path)
+    directory = os.path.dirname(path)
+    os.makedirs(directory, exist_ok=True)
+    fh = io.FileIO(path, mode='wb')
+
+    # Use MediaIoBaseDownload to download in chunks
+    downloader = MediaIoBaseDownload(fh, request, chunksize=1024 * 1024 * 100)
+
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+        print(f"Downloaded {status.progress() * 100}%")
+
+    fh.close()
+
+
+def download_deprecated(file, root_dir, dry_run):
     if not dry_run:
         path = os.path.join(root_dir, file['title'])
         path = os.path.join('/input', path)
